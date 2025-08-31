73bf71e72d2f830236745e07c280da0a8769b99f
@_a_u#JS <164733121+Au12jp@users.noreply.github.com>
2025-08-31 21:43:05 +0900

    Update forceShow.js

--- forceShow.js
+++ forceShow.js
@@ -1,7 +1,7 @@
 import * as MC from "@minecraft/server";
 import * as UI from "@minecraft/server-ui";
 
-const { world, system } = MC;
+const { system } = MC;
 
 /**
  * 
@@ -13,19 +13,19 @@ const { world, system } = MC;
 export function forceShow(player, form, timeout_ms = 1000) {
     return new Promise((resolve, reject) => {
         const start = Date.now();
-        
+
         const i = system.runInterval(async () => {
-            if((Date.now() - start) > timeout_ms) {
+            if ((Date.now() - start) > timeout_ms) {
                 reject(new Error(`${player.name} にフォームを強制送信していましたが、タイムアウトしました。`));
                 return;
             }
 
             const result = await form.show(player);
-            
+
             if (result.cancelationReason && result.cancelationReason === UI.FormCancelationReason.UserBusy) {
                 return;
             }
-    
+
             system.clearRun(i);
             resolve(result);
         }, 10);
